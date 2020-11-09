ed88f11ef872c62a76af32fcf18800e744eb4578
lo-tp <regesteraccount@hotmail.com>
2020-11-09 20:09:27 +0800

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,102 +1,114 @@
-import {useCallback, useEffect, useRef, useState} from "react"
+import { Ref, useCallback, useEffect, useRef, useState } from "react";
 
-export  enum IFrameStatus {
+export enum IFrameStatus {
   LOADING,
   LOADED,
   FAILED,
 }
 
-export type listener = (data:any) => void
-export type setter = (data:any) => void
+export type Listener = (data: any) => void;
+export type Setter = (data: any) => void;
 
 export const useIFrameParent = ({
   delay,
   childDomain,
   listen,
-  count: initialCount
-}:{
-  delay: number,
-  childDomain: string,
-  listen?: listener 
-  count?: number 
-}) => {
-  const ref = useRef<HTMLIFrameElement>(null)
-  const [status, setStatus] = useState(IFrameStatus.LOADING)
-  const timerId=useRef(0)
-  const count=useRef(initialCount||3)
+  count: initialCount,
+}: {
+  delay: number;
+  childDomain: string;
+  listen?: Listener;
+  count?: number;
+}): {
+  status: IFrameStatus;
+  onLoad: () => void;
+  ref: Ref<HTMLIFrameElement>;
+  send: (data: any) => void;
+} => {
+  const ref = useRef<HTMLIFrameElement>(null);
+  const [status, setStatus] = useState(IFrameStatus.LOADING);
+  const timerId = useRef(0);
+  const count = useRef(initialCount || 3);
 
   const messageListener = useCallback(
     (event) => {
-      const { origin, data } = event
-      if(origin===childDomain && data.child ){
-        if(data['__init__']){
-          clearInterval(timerId.current)
-          setStatus(IFrameStatus.LOADED)
-          window.removeEventListener('message', messageListener)
-        }
-        else if(listen){
-          listen(data.data)
+      const { origin, data } = event;
+      if (origin === childDomain && data.child) {
+        if (data.__init__) {
+          clearInterval(timerId.current);
+          setStatus(IFrameStatus.LOADED);
+        } else if (listen) {
+          listen(data.data);
         }
       }
     },
-    [timerId, childDomain, status, listen]
-  )
+    [timerId, childDomain, listen]
+  );
 
   useEffect(() => {
-    if (timerId) {
-      window.addEventListener('message', messageListener)
-    }
+    window.addEventListener("message", messageListener);
     return () => {
-      window.removeEventListener('message', messageListener)
-    }
-  }, [messageListener, timerId])
+      window.removeEventListener("message", messageListener);
+    };
+  }, [messageListener]);
 
   const onLoad = () => {
-      timerId.current=window.setInterval(() => {
-        if (count.current===0){
-          setStatus(IFrameStatus.FAILED)
-          window.removeEventListener('message', messageListener)
-          clearInterval(timerId.current)
-        } else {
-          ref?.current?.contentWindow?.postMessage({ parent: true, '__init__': '__init__' }, childDomain)
-          count.current=count.current-1
-        }
-      }, delay)
-  }
+    count.current = initialCount || 3;
+    timerId.current = window.setInterval(() => {
+      if (count.current === 0) {
+        setStatus(IFrameStatus.FAILED);
+        clearInterval(timerId.current);
+      } else {
+        ref?.current?.contentWindow?.postMessage(
+          { parent: true, __init__: "__init__" },
+          childDomain
+        );
+        count.current -= 1;
+      }
+    }, delay);
+  };
 
   return {
     status,
     onLoad,
     ref,
-    send: (data:any) => ref.current?.contentWindow?.postMessage({parent:true, data},childDomain)
-  }
-}
+    send: (data: any) =>
+      ref.current?.contentWindow?.postMessage(
+        { parent: true, data },
+        childDomain
+      ),
+  };
+};
 
 export const useIFrameChild = (
   parentDomain: string,
-  listen?: listener 
-) => {
-  const [initialized, setInitialized]=useState(false)
-
-  const messageListener=useCallback((event:MessageEvent) => {
-      const {origin,data}=event;
-      if (origin===parentDomain && data.parent){
-          if(data['__init__']) {
-              setInitialized(true)
-              window.parent.postMessage({'__init__':'__init__',child:true},parentDomain);
-          } else if(listen) {
-            listen(data.data)
-          }
+  listen?: Listener
+): { send: (data: any) => void } => {
+  const messageListener = useCallback(
+    (event: MessageEvent) => {
+      const { origin, data } = event;
+      if (origin === parentDomain && data.parent) {
+        if (data.__init__) {
+          window.parent.postMessage(
+            { __init__: "__init__", child: true },
+            parentDomain
+          );
+        } else if (listen) {
+          listen(data.data);
+        }
       }
-    },[initialized,parentDomain,listen])
+    },
+    [parentDomain, listen]
+  );
 
   useEffect(() => {
-    window.addEventListener('message',messageListener);
+    window.addEventListener("message", messageListener);
     return () => {
-      window.removeEventListener('message', messageListener)
-    }
-  },[messageListener]);
+      window.removeEventListener("message", messageListener);
+    };
+  }, [messageListener]);
   return {
-    send: (data: any) => window.parent.postMessage({child:true, data},parentDomain)
-  }
-}
+    send: (data: any) =>
+      window.parent.postMessage({ child: true, data }, parentDomain),
+  };
+};
