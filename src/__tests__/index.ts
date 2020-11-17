562d43d16f21edbf8449dc7eb05668a762cbbbba
lo-tp <regesteraccount@hotmail.com>
2020-11-17 14:47:52 +0800

    write the UT

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,3 +1,108 @@
-test('My Greeter', () => {
-  expect(1).toBe(2);
+import {
+  act,
+  renderHook,
+  RenderHookResult,
+} from "@testing-library/react-hooks";
+import { Listener, useIFrameChild } from "..";
+
+// test("should invoke the listen callback properly when receiving a non-init message", () => {
+
+describe("useIFrameChild shold work properly", () => {
+  const listen = jest.fn();
+  const parentDomain = "http://www.parentDomain.com";
+  let messageListener: (event: MessageEvent) => void;
+  let mockedPostMessage: jest.Mock;
+  let hook: RenderHookResult<
+    {
+      parentDomain: string;
+      listen?: Listener;
+    },
+    { send: (data: any) => void }
+  >;
+
+  beforeAll(() => {
+    global.addEventListener = jest.fn();
+    global.parent.postMessage = jest.fn();
+    hook = renderHook(() =>
+      useIFrameChild({
+        listen,
+        parentDomain,
+      })
+    );
+    const mockedAddEventListener = global.addEventListener as jest.Mock;
+    mockedPostMessage = global.parent.postMessage as jest.Mock;
+    [[, messageListener]] = mockedAddEventListener.mock.calls.filter(
+      (call) => call[0] === "message"
+    );
+  });
+
+  beforeEach(() => {
+    mockedPostMessage.mockClear();
+    listen.mockClear();
+  });
+
+  test("should respond to initial query from parent", () => {
+    const data = {
+      parent: true,
+      __init__: true,
+    };
+    act(() =>
+      messageListener(
+        new MessageEvent("message", {
+          origin: parentDomain,
+          data,
+        })
+      )
+    );
+    expect(mockedPostMessage.mock.calls).toEqual([
+      [
+        {
+          __init__: "__init__",
+          child: true,
+        },
+        parentDomain,
+      ],
+    ]);
+  });
+
+  test("should invoke the listen callback properly when receiving a non-init message", () => {
+    const data = {
+      parent: true,
+      data: "data strings",
+    };
+    act(() =>
+      messageListener(
+        new MessageEvent("message", {
+          origin: parentDomain,
+          data,
+        })
+      )
+    );
+    expect(mockedPostMessage.mock.calls).toEqual([]);
+    expect(listen.mock.calls).toEqual([["data strings"]]);
+  });
+
+  test("should do nothing when receiving a message not from the parent", () => {
+    const data = {
+      parent: true,
+      data: "data strings",
+    };
+    act(() =>
+      messageListener(
+        new MessageEvent("message", {
+          origin: "fake domain",
+          data,
+        })
+      )
+    );
+    expect(mockedPostMessage.mock.calls).toEqual([]);
+    expect(listen.mock.calls).toEqual([]);
+  });
+  test("should send message to parent properly", () => {
+    const data = "data string";
+    hook.result.current.send(data);
+    expect(mockedPostMessage.mock.calls).toEqual([
+      [{ child: true, data }, parentDomain],
+    ]);
+  });
 });
