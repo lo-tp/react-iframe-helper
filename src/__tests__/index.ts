543ad862793c1851aabcad5f922383a61e6ae127
lo-tp <regesteraccount@hotmail.com>
2020-11-30 16:59:33 +0800

    write UT to hande the case of failing to hande shake with the child

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -3,7 +3,18 @@ import {
   renderHook,
   RenderHookResult,
 } from "@testing-library/react-hooks";
-import { ChildProp, ChildResult, useIFrameChild } from "..";
+import React from "react";
+import {
+  ChildProp,
+  ChildResult,
+  DEFAULT_COUNT,
+  DEFAULT_DELAY,
+  IFrameStatus,
+  ParentProp,
+  ParentResult,
+  useIFrameChild,
+  useIFrameParent,
+} from "..";
 
 describe("useIFrameChild shold work properly", () => {
   const listen = jest.fn();
@@ -14,6 +25,7 @@ describe("useIFrameChild shold work properly", () => {
   let hook: RenderHookResult<ChildProp, ChildResult>;
 
   beforeAll(() => {
+    global.setInterval = jest.fn();
     global.addEventListener = jest.fn();
     global.removeEventListener = jest.fn();
     global.parent.postMessage = jest.fn();
@@ -112,5 +124,75 @@ describe("useIFrameChild shold work properly", () => {
     expect(
       removeEventListener.mock.calls.filter(([type]) => type === "message")
     ).toEqual([["message", messageListener]]);
+  });
+});
+
+describe("useIframeParent should mark status as FAILED when fail to hand shake with the child for count times", () => {
+  const listen = jest.fn();
+  const childDomain = "http://www.childDomain.com";
+  const mockedPostMessage = jest.fn();
+  let setIntervalListener: jest.Mock;
+  let clearIntervalListener: jest.Mock;
+  let hook: RenderHookResult<ParentProp, ParentResult>;
+  beforeAll(() => {
+    global.clearInterval = jest.fn();
+    global.setInterval = jest.fn().mockReturnValue(5);
+    global.parent.postMessage = jest.fn();
+    const { useRef: realUseRef } = React;
+    // eslint-disable-next-line
+    // @ts-ignore
+    jest.spyOn(React, "useRef").mockImplementation((initialVal) =>
+      initialVal === null
+        ? {
+            current: { contentWindow: { postMessage: mockedPostMessage } },
+          }
+        : realUseRef("react")
+    );
+    hook = renderHook(() =>
+      useIFrameParent({
+        listen,
+        childDomain,
+      })
+    );
+    setIntervalListener = global.setInterval as jest.Mock;
+    clearIntervalListener = global.clearInterval as jest.Mock;
+  });
+  test("call setInterVal with the correct args", () => {
+    const {
+      result: {
+        current: { onLoad },
+      },
+    } = hook;
+    onLoad();
+    expect(setIntervalListener.mock.calls[0][1]).toBe(DEFAULT_DELAY);
+  });
+  test("should set status properly when fail to hand shake for over count times", () => {
+    const {
+      result: {
+        current: { onLoad },
+      },
+    } = hook;
+    onLoad();
+    let count = DEFAULT_COUNT;
+    const intervalCallback = setIntervalListener.mock.calls[0][0];
+    expect(clearIntervalListener.mock.calls).toEqual([]);
+    expect(mockedPostMessage.mock.calls).toEqual([]);
+    while (count) {
+      expect(hook.result.current.status).toBe(IFrameStatus.LOADING);
+      act(() => {
+        intervalCallback();
+      });
+      expect(mockedPostMessage.mock.calls).toEqual([
+        [{ parent: true, __init__: "__init__" }, childDomain],
+      ]);
+      mockedPostMessage.mockClear();
+      count -= 1;
+    }
+    act(() => {
+      intervalCallback();
+    });
+    expect(mockedPostMessage.mock.calls).toEqual([]);
+    expect(hook.result.current.status).toBe(IFrameStatus.FAILED);
+    expect(clearIntervalListener.mock.calls).toEqual([[5]]);
   });
 });
